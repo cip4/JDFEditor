a37c5268212310e061e1c6e0a3cb5ae0e953d290
biskey <biskey@cip4.org>
2008-03-13 09:25:18 +0000

    When running DevCap file, it will export the XML report which can be viewed w/ and XML editor.
    git-svn-id: http://svn.cip4.org/svnroot/trunk@3196 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/java/org/cip4/jdfeditor/DeviceCapDialog.java
+++ src/java/org/cip4/jdfeditor/DeviceCapDialog.java
@@ -191,7 +191,7 @@ public class DeviceCapDialog extends JPanel implements ActionListener
                                 if(device==null)
                                 {
                                     JOptionPane.showMessageDialog(parent, 
-                                            "File does not contain JDF Node Device Capability description", 
+                                            "DevCap File does not contain JDF Node Device Capability description", 
                                             "Error", JOptionPane.ERROR_MESSAGE);
                                 }
                                 else
@@ -205,7 +205,12 @@ public class DeviceCapDialog extends JPanel implements ActionListener
                                     }
                                     executableJDF = device.getExecutableJDF((JDFNode)docRoot,testlists,validationLevel);
                                     bugReport = device.getBadJDFInfo((JDFNode)docRoot,testlists,validationLevel);
-
+                                    
+                                    //Output BugReport to file. Location is the location of the DevCap file.
+                                    String dcReport = null;
+                                    final String dcr = devCapDoc.getOriginalFileName();
+                                    dcReport = StringUtil.newExtension(dcr, ".JDFDevCapREPORT.xml");
+                                    bugReport.write2File(dcReport, 2, true);
                                 }
                             }
                             else if(docRoot instanceof JDFJMF)
@@ -214,7 +219,7 @@ public class DeviceCapDialog extends JPanel implements ActionListener
                                 if(ms==null)
                                 {
                                     JOptionPane.showMessageDialog(parent, 
-                                            "File does not contain JMF Device Capability description", 
+                                            "DevCap File does not contain JMF Device Capability description", 
                                             "Error", JOptionPane.ERROR_MESSAGE);
                                 }
                                 else
@@ -224,6 +229,10 @@ public class DeviceCapDialog extends JPanel implements ActionListener
 
                                     executableJDF = null;
                                     bugReport = JDFDeviceCap.getJMFInfo((JDFJMF)docRoot, respKnownMessages, testlists, validationLevel, !Editor.getIniFile().getHighlight());
+                                    
+                                    String dcr = devCapDoc.getOriginalFileName();
+                                    String dcReport = StringUtil.newExtension(dcr, ".JMFDevCapReport.xml");
+                                    bugReport.write2File(dcReport, 2, true);
                                 }
                             }
                         }         
