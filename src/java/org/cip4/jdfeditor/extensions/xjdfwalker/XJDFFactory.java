27f8b34eaa5383cd080c0c7ddd45269672b3fd43
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2008-11-10 12:04:50 +0000

    git-svn-id: http://svn.cip4.org/svnroot/trunk@3673 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ src/java/org/cip4/jdfeditor/extensions/xjdfwalker/XJDFFactory.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.cip4.jdflib.extensions.xjdfwalker;
+
+import org.cip4.jdflib.elementwalker.BaseWalkerFactory;
+
+/**
+ * @author Rainer Prosi, Heidelberger Druckmaschinen
+ * factory to convert 
+ */
+public class XJDFFactory extends BaseWalkerFactory
+{
+	//nop
+}
