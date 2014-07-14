4419671c775c8e0b2c7299e21017d40967a5ef52
Rainer Prosi <rainer.prosi@heidelberg.com>
2014-07-14 19:31:10 +0200

    added use of system proxy settings

--- src/main/java/org/cip4/tools/jdfeditor/App.java
+++ src/main/java/org/cip4/tools/jdfeditor/App.java
@@ -1,7 +1,12 @@
 package org.cip4.tools.jdfeditor;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.apache.commons.io.FilenameUtils;
 import org.apache.logging.log4j.LogManager;
+import org.cip4.jdflib.util.net.ProxyUtil;
 import org.cip4.tools.jdfeditor.controller.MainController;
 import org.cip4.tools.jdfeditor.util.DirectoryUtil;
 import org.springframework.context.ApplicationContext;
@@ -9,84 +14,87 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Properties;
-
 /**
  * Initialize and start JDFEditor Application.
  */
 
 @Configuration
 @ComponentScan
-public class App {
+public class App
+{
 
-    public static final String APP_NAME = getBuildProp("name");
+	public static final String APP_NAME = getBuildProp("name");
 
-    public static final String APP_VERSION = getBuildProp("version");
+	public static final String APP_VERSION = getBuildProp("version");
 
-    public static final String APP_RELEASE_DATE = getBuildProp("build.date");
+	public static final String APP_RELEASE_DATE = getBuildProp("build.date");
 
-    private static final String RES_BUILD_PROPS = "/org/cip4/tools/jdfeditor/build.properties";
+	private static final String RES_BUILD_PROPS = "/org/cip4/tools/jdfeditor/build.properties";
 
-    /**
-     * Application main entrance point.
-     * @param args
-     */
-    public static void main(final String[] args)
-    {
-        // apple menu compatibility
-        System.setProperty("apple.laf.useScreenMenuBar", "true");
+	/**
+	 * Application main entrance point.
+	 * @param args
+	 */
+	public static void main(final String[] args)
+	{
+		// apple menu compatibility
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
 
-        // init logging
-        String logFile = FilenameUtils.concat(DirectoryUtil.getDirCIP4Tools(), "logs");
-        logFile = FilenameUtils.concat(logFile, "JDFEditor.log");
+		// init logging
+		String logFile = FilenameUtils.concat(DirectoryUtil.getDirCIP4Tools(), "logs");
+		logFile = FilenameUtils.concat(logFile, "JDFEditor.log");
 
-        System.setProperty("filename", logFile);
-        LogManager.getLogger(App.class).info("--- Start CIP4 JDFEditor ------------------------------ ");
+		System.setProperty("filename", logFile);
+		LogManager.getLogger(App.class).info("--- Start CIP4 JDFEditor ------------------------------ ");
 
-        // handle command line arguments
-        File file = null;
-        // mac may have 2nd argument
-        for (int i = args.length - 1; i >= 0; i--)
-        {
-            if (!args[i].startsWith("-"))
-            {
-                file = new File(args[i]);
-                if (file.canRead())
-                {
-                    break;
-                }
-                file = null;
-            }
-        }
+		// update proxies to system
+		ProxyUtil.setUseSystemDefault(true);
 
-        // show main view
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
-        MainController mainController = ctx.getBean(MainController.class);
-        mainController.displayForm(file);
-    }
+		// handle command line arguments
+		File file = null;
 
-    /**
-     * Read and returns a build property by key.
-     * @param key The key of the build property.
-     * @return The value of the build property by key.
-     */
-    private static String getBuildProp(String key) {
+		// mac may have 2nd argument
+		for (int i = args.length - 1; i >= 0; i--)
+		{
+			if (!args[i].startsWith("-"))
+			{
+				file = new File(args[i]);
+				if (file.canRead())
+				{
+					break;
+				}
+				file = null;
+			}
+		}
 
-        Properties props = new Properties();
+		// show main view
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
+		MainController mainController = ctx.getBean(MainController.class);
+		mainController.displayForm(file);
+	}
 
-        try {
-            props.load(App.class.getResourceAsStream(RES_BUILD_PROPS));
+	/**
+	 * Read and returns a build property by key.
+	 * @param key The key of the build property.
+	 * @return The value of the build property by key.
+	 */
+	private static String getBuildProp(String key)
+	{
 
-        } catch (IOException e) {
-            LogManager.getLogger(App.class).error("Error loading build properties.", e);
-        }
+		Properties props = new Properties();
 
-        String value = props.getProperty(key);
+		try
+		{
+			props.load(App.class.getResourceAsStream(RES_BUILD_PROPS));
 
-        return value;
-    }
+		}
+		catch (IOException e)
+		{
+			LogManager.getLogger(App.class).error("Error loading build properties.", e);
+		}
+
+		String value = props.getProperty(key);
+
+		return value;
+	}
 }
-
-
